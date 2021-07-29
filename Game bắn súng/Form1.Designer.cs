fea60b8010f72de36cc7ef43d403839230d4d60b
Huynh Thiên Long <huynhnen687@gmail.com>
2021-07-29 21:34:51 +0700

    Thieu boss cuoi

---
+++ Game bắn súng/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace Game_bắn_súng
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.NgoiSao = new System.Windows.Forms.Timer(this.components);
+            this.NguoiChoi = new System.Windows.Forms.PictureBox();
+            this.TraiMoveTimer = new System.Windows.Forms.Timer(this.components);
+            this.PhaiMoveTimer = new System.Windows.Forms.Timer(this.components);
+            this.XuongMoveTimer = new System.Windows.Forms.Timer(this.components);
+            this.LenMoveTimer = new System.Windows.Forms.Timer(this.components);
+            this.DanMoveTimer = new System.Windows.Forms.Timer(this.components);
+            this.QuaiVatMoveTimer = new System.Windows.Forms.Timer(this.components);
+            this.DanQuaiVatMoveTimer = new System.Windows.Forms.Timer(this.components);
+            this.TuaDe = new System.Windows.Forms.Label();
+            this.btnDungTranDau = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblDiem = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblCap = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.NguoiChoi)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // NgoiSao
+            // 
+            this.NgoiSao.Enabled = true;
+            this.NgoiSao.Interval = 10;
+            this.NgoiSao.Tick += new System.EventHandler(this.NgoiSao_Tick);
+            // 
+            // NguoiChoi
+            // 
+            this.NguoiChoi.Image = ((System.Drawing.Image)(resources.GetObject("NguoiChoi.Image")));
+            this.NguoiChoi.Location = new System.Drawing.Point(263, 379);
+            this.NguoiChoi.Name = "NguoiChoi";
+            this.NguoiChoi.Size = new System.Drawing.Size(58, 50);
+            this.NguoiChoi.TabIndex = 0;
+            this.NguoiChoi.TabStop = false;
+            // 
+            // TraiMoveTimer
+            // 
+            this.TraiMoveTimer.Tick += new System.EventHandler(this.TraiMoveTimer_Tick);
+            // 
+            // PhaiMoveTimer
+            // 
+            this.PhaiMoveTimer.Tick += new System.EventHandler(this.PhaiMoveTimer_Tick);
+            // 
+            // XuongMoveTimer
+            // 
+            this.XuongMoveTimer.Tick += new System.EventHandler(this.XuongMoveTimer_Tick);
+            // 
+            // LenMoveTimer
+            // 
+            this.LenMoveTimer.Tick += new System.EventHandler(this.LenMoveTimer_Tick);
+            // 
+            // DanMoveTimer
+            // 
+            this.DanMoveTimer.Enabled = true;
+            this.DanMoveTimer.Interval = 20;
+            this.DanMoveTimer.Tick += new System.EventHandler(this.DanMoveTimer_Tick);
+            // 
+            // QuaiVatMoveTimer
+            // 
+            this.QuaiVatMoveTimer.Enabled = true;
+            this.QuaiVatMoveTimer.Tick += new System.EventHandler(this.QuaiVatMoveTimer_Tick);
+            // 
+            // DanQuaiVatMoveTimer
+            // 
+            this.DanQuaiVatMoveTimer.Enabled = true;
+            this.DanQuaiVatMoveTimer.Interval = 20;
+            this.DanQuaiVatMoveTimer.Tick += new System.EventHandler(this.DanQuaiVatMoveTimer_Tick);
+            // 
+            // TuaDe
+            // 
+            this.TuaDe.AutoSize = true;
+            this.TuaDe.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TuaDe.ForeColor = System.Drawing.Color.Red;
+            this.TuaDe.Location = new System.Drawing.Point(269, 191);
+            this.TuaDe.Name = "TuaDe";
+            this.TuaDe.Size = new System.Drawing.Size(122, 45);
+            this.TuaDe.TabIndex = 1;
+            this.TuaDe.Text = "label1";
+            this.TuaDe.Visible = false;
+            // 
+            // btnDungTranDau
+            // 
+            this.btnDungTranDau.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDungTranDau.Location = new System.Drawing.Point(226, 31);
+            this.btnDungTranDau.Name = "btnDungTranDau";
+            this.btnDungTranDau.Size = new System.Drawing.Size(327, 95);
+            this.btnDungTranDau.TabIndex = 2;
+            this.btnDungTranDau.Text = " Trận Đấu Mới";
+            this.btnDungTranDau.UseVisualStyleBackColor = true;
+            this.btnDungTranDau.Visible = false;
+            this.btnDungTranDau.Click += new System.EventHandler(this.btnDungTranDau_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThoat.Location = new System.Drawing.Point(226, 298);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(329, 89);
+            this.btnThoat.TabIndex = 3;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Visible = false;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(5, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 32);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Điểm";
+            // 
+            // lblDiem
+            // 
+            this.lblDiem.AutoSize = true;
+            this.lblDiem.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDiem.Location = new System.Drawing.Point(108, 17);
+            this.lblDiem.Name = "lblDiem";
+            this.lblDiem.Size = new System.Drawing.Size(29, 32);
+            this.lblDiem.TabIndex = 5;
+            this.lblDiem.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(574, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 32);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Cấp";
+            // 
+            // lblCap
+            // 
+            this.lblCap.AutoSize = true;
+            this.lblCap.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCap.Location = new System.Drawing.Point(659, 17);
+            this.lblCap.Name = "lblCap";
+            this.lblCap.Size = new System.Drawing.Size(29, 32);
+            this.lblCap.TabIndex = 7;
+            this.lblCap.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(782, 453);
+            this.Controls.Add(this.lblCap);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblDiem);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnDungTranDau);
+            this.Controls.Add(this.TuaDe);
+            this.Controls.Add(this.NguoiChoi);
+            this.MinimumSize = new System.Drawing.Size(800, 500);
+            this.Name = "Form1";
+            this.Text = "Bắn quái vật";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.NguoiChoi)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer NgoiSao;
+        private System.Windows.Forms.PictureBox NguoiChoi;
+        private System.Windows.Forms.Timer TraiMoveTimer;
+        private System.Windows.Forms.Timer PhaiMoveTimer;
+        private System.Windows.Forms.Timer XuongMoveTimer;
+        private System.Windows.Forms.Timer LenMoveTimer;
+        private System.Windows.Forms.Timer DanMoveTimer;
+        private System.Windows.Forms.Timer QuaiVatMoveTimer;
+        private System.Windows.Forms.Timer DanQuaiVatMoveTimer;
+        private System.Windows.Forms.Label TuaDe;
+        private System.Windows.Forms.Button btnDungTranDau;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblDiem;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblCap;
+    }
+}
+
---
+++ Game bắn súng/Form1.cs
@@ -0,0 +1,421 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WMPLib;
+
+namespace Game_bắn_súng
+{
+    public partial class Form1 : Form
+    {
+        WindowsMediaPlayer NhacGame;
+        WindowsMediaPlayer NhacDan;
+        WindowsMediaPlayer BanChet;
+
+        PictureBox[] DanQuaiVat;
+        int TocDoDanQuaiVat;
+
+        PictureBox[] Sao;
+        int TocDoChoi;
+
+        PictureBox[] Dan;
+        int TocDoDan;
+
+        PictureBox[] QuaiVat;
+        int TocDoQuaiVat;
+
+        int TocDoNen;
+        Random NgauNhien;
+
+        int Diem;
+        int CapDo;
+        int DoKho;
+        bool TamDung;
+        bool TroChoiKetThuc;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            TamDung = false;
+            TroChoiKetThuc = false;
+            Diem = 0;
+            DoKho = 9;
+            CapDo = 1;
+
+            TocDoNen = 15;
+            TocDoChoi = 30;
+            TocDoQuaiVat = 15;
+            TocDoDan = 30;
+            TocDoDanQuaiVat = 10;
+
+            Dan = new PictureBox[3];
+
+
+            Image Danban = Image.FromFile(@"hinhanh\Danban.png");
+
+            Image QuaiVat1 = Image.FromFile("hinhanh\\boss1.jpg");
+            Image QuaiVat2 = Image.FromFile("hinhanh\\boss2.jpg");
+            Image QuaiVat3 = Image.FromFile("hinhanh\\boss3.jpg");
+            Image QuaiVat4 = Image.FromFile("hinhanh\\bossHead1.png");
+            Image QuaiVat5 = Image.FromFile("hinhanh\\BossHead2.jpg");
+
+            QuaiVat = new PictureBox[10];
+
+            for (int i = 0; i < QuaiVat.Length; i++)
+            {
+                QuaiVat[i] = new PictureBox();
+                QuaiVat[i].Size = new Size(40, 40);
+                QuaiVat[i].SizeMode = PictureBoxSizeMode.Zoom;
+                QuaiVat[i].BorderStyle = BorderStyle.None;
+                QuaiVat[i].Visible = false;
+                this.Controls.Add(QuaiVat[i]);
+                QuaiVat[i].Location = new Point((i + 1) * 50, -50);
+            }
+
+            QuaiVat[0].Image = QuaiVat4;
+            QuaiVat[1].Image = QuaiVat2;
+            QuaiVat[2].Image = QuaiVat3;
+            QuaiVat[3].Image = QuaiVat3;
+            QuaiVat[4].Image = QuaiVat1;
+            QuaiVat[5].Image = QuaiVat3;
+            QuaiVat[6].Image = QuaiVat2;
+            QuaiVat[7].Image = QuaiVat3;
+            QuaiVat[8].Image = QuaiVat2;
+            QuaiVat[9].Image = QuaiVat5;
+
+            for (int i=0; i<Dan.Length; i++)
+            {
+                Dan[i] = new PictureBox();
+                Dan[i].Size = new Size(8, 8);
+                Dan[i].Image = Danban;
+                Dan[i].SizeMode = PictureBoxSizeMode.Zoom;
+                Dan[i].BorderStyle = BorderStyle.None;
+                this.Controls.Add(Dan[i]);
+            }
+
+            NhacGame = new WindowsMediaPlayer();
+            NhacDan = new WindowsMediaPlayer();
+            BanChet = new WindowsMediaPlayer();
+
+            NhacGame.URL = "nhac\\nhacnen.mp3";
+            NhacDan.URL = "nhac\\dan.mp3";
+            BanChet.URL = "nhac\\bom.mp3";
+
+            NhacGame.settings.setMode("loop", true);
+            NhacGame.settings.volume = 5;
+            NhacDan.settings.volume = 1;
+            BanChet.settings.volume = 6;
+
+            Sao = new PictureBox[10];
+            NgauNhien = new Random();
+
+            for(int i = 0; i<Sao.Length; i++)
+            {
+                Sao[i] = new PictureBox();
+                Sao[i].BorderStyle = BorderStyle.None;
+                Sao[i].Location = new Point(NgauNhien.Next(20, 580), NgauNhien.Next(-10, 400));
+                if(i % 2 == 1)
+                {
+                    Sao[i].Size = new Size(4, 4);
+                    Sao[i].BackColor = Color.Wheat;
+                }
+                else
+                {
+                    Sao[i].Size = new Size(4, 4);
+                    Sao[i].BackColor = Color.Red;
+                }
+
+                this.Controls.Add(Sao[i]);              
+            }
+
+            DanQuaiVat = new PictureBox[10];
+            Image DanQuaiVatBan = Image.FromFile(@"hinhanh\DanQuaiVat.png");
+
+            for (int i = 0; i < DanQuaiVat.Length; i++)
+            {
+                DanQuaiVat[i] = new PictureBox();
+                DanQuaiVat[i].Size = new Size(8, 15);
+                DanQuaiVat[i].Image = DanQuaiVatBan;
+                DanQuaiVat[i].SizeMode = PictureBoxSizeMode.Zoom;
+                //DanQuaiVat[i].Visible = false;
+                //DanQuaiVat[i].BackColor = Color.Red;
+                int x = NgauNhien.Next(0, 10);
+                DanQuaiVat[i].BorderStyle = BorderStyle.None;
+                DanQuaiVat[i].Location = new Point(QuaiVat[x].Location.X, QuaiVat[x].Location.Y - 20);
+                this.Controls.Add(DanQuaiVat[i]);
+            }
+            NhacGame.controls.play();
+        }
+
+        private void NgoiSao_Tick(object sender, EventArgs e)
+        {
+            for (int i =0; i< Sao.Length/2; i++)
+            {
+                Sao[i].Top += TocDoNen;
+                if(Sao[i].Top >= this.Height)
+                {
+                    Sao[i].Top = -Sao[i].Height;
+                }
+            }
+            for (int i = Sao.Length/2; i < Sao.Length; i++)
+            {
+                Sao[i].Top += TocDoNen - 2;
+                if (Sao[i].Top >= this.Height)
+                {
+                    Sao[i].Top = -Sao[i].Height;
+                }
+            }
+        }
+
+        private void TraiMoveTimer_Tick(object sender, EventArgs e)
+        {
+            if(NguoiChoi.Left > 10)
+            {
+                NguoiChoi.Left -= TocDoChoi;
+            }
+        }
+
+        private void PhaiMoveTimer_Tick(object sender, EventArgs e)
+        {
+            if (NguoiChoi.Right < 580)
+            {
+                NguoiChoi.Left += TocDoChoi;
+            }
+        }
+
+        private void XuongMoveTimer_Tick(object sender, EventArgs e)
+        {
+            if (NguoiChoi.Top < 400)
+            {
+                NguoiChoi.Top += TocDoChoi;
+            }
+        }
+
+        private void LenMoveTimer_Tick(object sender, EventArgs e)
+        {
+            if (NguoiChoi.Top > 10)
+            {
+                NguoiChoi.Top -= TocDoChoi;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (!TamDung)
+            {
+                if (e.KeyCode == Keys.Right)
+                {
+                    PhaiMoveTimer.Start();
+                }
+                if (e.KeyCode == Keys.Left)
+                {
+                    TraiMoveTimer.Start();
+                }
+                if (e.KeyCode == Keys.Down)
+                {
+                    XuongMoveTimer.Start();
+                }
+                if (e.KeyCode == Keys.Up)
+                {
+                    LenMoveTimer.Start();
+                }
+            }
+
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            PhaiMoveTimer.Stop();
+            TraiMoveTimer.Stop();
+            XuongMoveTimer.Stop();
+            LenMoveTimer.Stop();
+
+            if (e.KeyCode == Keys.Space)
+            {
+                if (!TroChoiKetThuc)
+                {
+                    if (TamDung)
+                    {
+                        BatDauThoiGian();
+                        TuaDe.Visible = false;
+                        NhacGame.controls.play();
+                        TamDung = false;
+                    }
+                    else
+                    {
+                        TuaDe.Location = new Point(this.Width / 2 - 120, 150);
+                        TuaDe.Text = "TẠM DỪNG";
+                        TuaDe.Visible = true;
+                        NhacGame.controls.pause();
+                        DungThoiGian();
+                        TamDung = true;
+                    }
+                }
+            }
+        }
+
+        private void DanMoveTimer_Tick(object sender, EventArgs e)
+        {
+            NhacDan.controls.play();
+            for (int i = 0; i< Dan.Length; i++)
+            {
+                if (Dan[i].Top > 0)
+                {
+                    Dan[i].Visible = true;
+                    Dan[i].Top -= TocDoDan;
+
+                    HieuUng();
+                }
+                else
+                {
+                    Dan[i].Visible = false;
+                    Dan[i].Location = new Point(NguoiChoi.Location.X + 20, NguoiChoi.Location.Y - i * 30);
+                }
+            }
+        }
+
+        private void QuaiVatMoveTimer_Tick(object sender, EventArgs e)
+        {
+            QuaiVatChuyenDong(QuaiVat, TocDoQuaiVat);
+        }
+
+        private void QuaiVatChuyenDong(PictureBox[] Mang, int TocDo)
+        {
+            for (int i = 0; i < Mang.Length; i++)
+            {
+                Mang[i].Visible = true;
+                Mang[i].Top += TocDo;
+                if(Mang[i].Top > this.Height)
+                {
+                    Mang[i].Location = new Point((i + 1) * 50, -200);
+                }
+            }
+        }
+
+        private void HieuUng()
+        {
+            for (int i = 0; i < QuaiVat.Length; i++)
+            {
+                if (Dan[0].Bounds.IntersectsWith(QuaiVat[i].Bounds)
+                    || Dan[1].Bounds.IntersectsWith(QuaiVat[i].Bounds) || Dan[2].Bounds.IntersectsWith(QuaiVat[i].Bounds))
+                {
+                    BanChet.controls.play();
+
+#Tínhđiểm
+                    Diem = Diem + 1;
+                    lblDiem.Text = (Diem < 10) ? "0" + Diem.ToString() : Diem.ToString();
+
+                    if (Diem % 10 == 0)
+                    {
+                        CapDo = CapDo + 1;
+                        lblCap.Text = (CapDo < 10) ? "0" + CapDo.ToString() : CapDo.ToString();
+
+                        if (TocDoQuaiVat <= 10 && TocDoDanQuaiVat <= 10 && DoKho >= 0)
+                        {
+                            DoKho--;
+                            TocDoQuaiVat++;
+                            TocDoDanQuaiVat++;
+                        }
+
+                        if (CapDo == 10)
+                        {
+                            GameKetThuc("BẠN CHƠI RẤT TUYỆT");
+                        }
+                    }
+
+                    QuaiVat[i].Location = new Point((i + 1) * 50, -100);
+                }
+
+                if (NguoiChoi.Bounds.IntersectsWith(QuaiVat[i].Bounds))
+                {
+                    BanChet.settings.volume = 30;
+                    BanChet.controls.play();
+                    NguoiChoi.Visible = false;
+                    GameKetThuc("GAME OVER");
+                }
+            }
+        }
+
+        private void GameKetThuc(String str)
+        {
+            TuaDe.Text = str;
+            //TuaDe.Location = new Point(120, 120);
+            TuaDe.Visible = true;
+            btnDungTranDau.Visible = true;
+            btnThoat.Visible = true;
+
+            NhacGame.controls.stop();
+            DungThoiGian();
+        }
+
+        private void DungThoiGian()
+        {
+            NgoiSao.Stop();
+            QuaiVatMoveTimer.Stop();
+            DanMoveTimer.Stop();
+            DanQuaiVatMoveTimer.Stop();
+        }
+
+        private void BatDauThoiGian()
+        {
+            NgoiSao.Start();
+            QuaiVatMoveTimer.Start();
+            DanMoveTimer.Start();
+        }
+
+        private void DanQuaiVatMoveTimer_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < (DanQuaiVat.Length - DoKho); i++)
+            {
+                if (DanQuaiVat[i].Top < this.Height)
+                {
+                    DanQuaiVat[i].Visible = true;
+                    DanQuaiVat[i].Top += TocDoDanQuaiVat;
+                    HieuUngDongCuaDan();
+                }
+                else
+                {
+                    DanQuaiVat[i].Visible = false;
+                    int x = NgauNhien.Next(0, 10);
+                    DanQuaiVat[i].Location = new Point(QuaiVat[x].Location.X + 20, QuaiVat[x].Location.Y + 30);
+                }
+            }
+        }
+
+        private void HieuUngDongCuaDan()
+        {
+            for (int i = 0; i < DanQuaiVat.Length; i++)
+            {
+                if (DanQuaiVat[i].Bounds.IntersectsWith(NguoiChoi.Bounds))
+                {
+                    DanQuaiVat[i].Visible = false;
+                    BanChet.settings.volume = 30;
+                    BanChet.controls.play();
+                    NguoiChoi.Visible = false;
+                    GameKetThuc("GAME OVER");
+                }
+            }
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnDungTranDau_Click(object sender, EventArgs e)
+        {
+            this.Controls.Clear();
+            InitializeComponent();
+            Form1_Load(e, e);
+        }
+    }
+}
